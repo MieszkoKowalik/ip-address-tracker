0444301a6ad8641707a6b41e012e0f1ceb36c025
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-10-12 21:25:33 +0200

    add colapse function to tracker__results for better mobile experience

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,9 @@ import * as L from "leaflet";
 import Tracker from "./Tracker";
 
 new Tracker();
+
+const colapseBtn = document.querySelector("[data-btnColapse]");
+
+colapseBtn.addEventListener("click", () => {
+  document.querySelector(".tracker__results").classList.toggle("isColapsed");
+});
